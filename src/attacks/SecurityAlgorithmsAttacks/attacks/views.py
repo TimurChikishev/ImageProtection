30c9cf9bbcd1af76fe13f11e50832bf76bcd46fc
Тимур <swallow.chikishev@gmail.com>
2021-06-16 22:13:03 +0300

    Вычислите средний индекс структурного сходства между двумя изображениями.

--- src/attacks/SecurityAlgorithmsAttacks/attacks/views.py
+++ src/attacks/SecurityAlgorithmsAttacks/attacks/views.py
@@ -7,9 +7,10 @@ from django.core.files.storage import FileSystemStorage
 from django.contrib import messages
 from .attack.modules.analysis import Analyzer
 from .attack.hist_test import *
-import skimage 
+from skimage.metrics import structural_similarity as ssim
 from pathlib import Path
 import os
+import numpy as np
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/media')
@@ -18,33 +19,57 @@ an = Analyzer()
 @csrf_exempt
 def index(request):
     context = {}
-    if request.method == "POST":
-        img = request.FILES['uploadFromPC']
-        print(img)
-        
-        if isValidateImage(img.name):
-            fs = FileSystemStorage()
-            name = fs.save(img.name, img)
-            url = fs.url(name)
-            context['image_url'] = url
+    try:
+        if request.method == "POST":
+            img = request.FILES['uploadFromPC']
+            print(img)
             
-            mod_name = protectImage(img)
-            mod_name_url = "/media/"+mod_name
-            context['hist1'] = "/media/"+"hist_source_"+img.name
-            context['hist2'] = "/media/"+"hist1_"+img.name
-            context['mod_image_url'] = mod_name_url
-            
-            an.visual_attack(Image.open(MEDIA_ROOT+"/"+img.name), join=True)
-            context['src_lsb_image_url'] = "/media/"+img.name.split(".")[0]+"_LSB.bmp"
-            an.visual_attack(Image.open(MEDIA_ROOT+"/"+mod_name), join=True)
-            context['mod_lsb_image_url'] = "/media/"+mod_name.split(".")[0]+"_LSB.bmp"
-        else:
-            messages.warning(request, 'Файл должен иметь разрешение png или jpg!')
-            print("VALIDATE ERROR")
+            if isValidateImage(img.name):
+                fs = FileSystemStorage()
+                name = fs.save(img.name, img)
+                url = fs.url(name)
+                context['image_url'] = url
+                
+                mod_name = protectImage(img)
+                mod_name_url = "/media/"+mod_name
+                context['hist1'] = "/media/"+"hist_source_"+img.name
+                context['hist2'] = "/media/"+"hist1_"+img.name
+                context['mod_image_url'] = mod_name_url
+                
+                an.visual_attack(Image.open(MEDIA_ROOT+"/"+img.name), join=True)
+                context['src_lsb_image_url'] = "/media/"+img.name.split(".")[0]+"_LSB.bmp"
+                an.visual_attack(Image.open(MEDIA_ROOT+"/"+mod_name), join=True)
+                context['mod_lsb_image_url'] = "/media/"+mod_name.split(".")[0]+"_LSB.bmp"
+            else:
+                messages.warning(request, 'Файл должен иметь разрешение png или jpg!')
+                
+    except Exception as ex:
+        messages.warning(request, str(ex))
         
     return render(request, "attacks/index.html", context)
 
 @csrf_exempt
 def comparison(request):
-    return render(request, "analysis/comparison_image.html")
+    context = {}
+    if request.method == "POST":
+        try:
+            fs = FileSystemStorage()      
+            img = request.FILES
+            img_list = img.getlist('uploadFromPC[]')
+            
+            name1 = fs.save(img_list[0].name, img_list[0])
+            name2 = fs.save(img_list[1].name, img_list[1])
+
+            img1 = Image.open(MEDIA_ROOT+"/"+img_list[0].name)
+            img2 = Image.open(MEDIA_ROOT+"/"+img_list[1].name)
+            img_a = np.asarray(img1)
+            img_b = np.asarray(img2)
+  
+            ssimg = ssim(img_a, img_b, multichannel=True) 
+            context['ssim'] = ssimg
+            
+        except Exception as ex:
+            messages.warning(request, str(ex))
+            
+    return render(request, "analysis/comparison_image.html", context)
 
---
+++ src/attacks/SecurityAlgorithmsAttacks/static/js/comparison_image.js
@@ -0,0 +1,54 @@
+var file_list = []
+var dt = new DataTransfer();
+
+function readURL(input) {
+
+    console.log(input.files)
+    if (file_list.length < 2) {
+        dt.items.add(input.files[0]);
+        file_list = dt.files;
+    } else {
+        removeUpload();
+    }
+    // console.log(input.files[0])
+    // console.log(file_list);
+    if (input.files.length < file_list.length)
+        input.files = file_list
+
+    if (input.files && file_list[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function(e) {
+            if (file_list.length == 2)
+                $('.image-upload-wrap').hide();
+
+            $('.file-upload-image' + file_list.length).attr('src', e.target.result);
+            $('.file-upload-content').show();
+
+            // $('.image-title').html(input.files.name);
+        };
+
+
+        reader.readAsDataURL(file_list[file_list.length - 1]);
+
+
+    } else {
+        removeUpload();
+    }
+}
+
+function removeUpload() {
+    file_list = [];
+    dt.clearData();
+    document.getElementById('image1').src = "";
+    document.getElementById('image2').src = "";
+    $('.file-upload-input').replaceWith($('.file-upload-input').clone());
+    $('.file-upload-content').hide();
+    $('.image-upload-wrap').show();
+}
+$('.image-upload-wrap').bind('dragover', function() {
+    $('.image-upload-wrap').addClass('image-dropping');
+});
+$('.image-upload-wrap').bind('dragleave', function() {
+    $('.image-upload-wrap').removeClass('image-dropping');
+});
